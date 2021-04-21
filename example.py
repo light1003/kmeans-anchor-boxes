530c18eeca2b91fb1ef1f75f3b6ef52e220ac47e
Vinicio Valbuena <vinicio.valbuena89@gmail.com>
2021-04-21 23:30:25 +0200

    add num obj

--- example.py
+++ example.py
@@ -9,25 +9,27 @@ ANNOTATIONS_PATH = "Annotations"
 CLUSTERS = 5
 
 def load_dataset(path):
-	dataset = []
-	for xml_file in glob.glob("{}/*xml".format(path)):
-		tree = ET.parse(xml_file)
+    dataset = []
+    for xml_file in glob.glob("{}/*xml".format(path)):
+        tree = ET.parse(xml_file)
 
-		height = int(tree.findtext("./size/height"))
-		width = int(tree.findtext("./size/width"))
+        height = int(tree.findtext("./size/height"))
+        width = int(tree.findtext("./size/width"))
 
-		for obj in tree.iter("object"):
-			xmin = int(obj.findtext("bndbox/xmin"))*1.0 / width
-			ymin = int(obj.findtext("bndbox/ymin"))*1.0 / height
-			xmax = int(obj.findtext("bndbox/xmax"))*1.0 / width
-			ymax = int(obj.findtext("bndbox/ymax"))*1.0 / height
-			if xmin==xmax or ymin==ymax:
-				print("you need to check {}".format(xml_file))
-				continue
+        i = 0
+        for obj in tree.iter("object"):
+            i += 1
+            xmin = int(obj.findtext("bndbox/xmin"))*1.0 / width
+            ymin = int(obj.findtext("bndbox/ymin"))*1.0 / height
+            xmax = int(obj.findtext("bndbox/xmax"))*1.0 / width
+            ymax = int(obj.findtext("bndbox/ymax"))*1.0 / height
+            if xmin==xmax or ymin==ymax:
+                print("you need to check obj[{}] {}".format(i, xml_file))
+                continue
 
-			dataset.append([xmax - xmin, ymax - ymin])
+            dataset.append([xmax - xmin, ymax - ymin])
 
-	return np.array(dataset)
+    return np.array(dataset)
 
 
 data = load_dataset(ANNOTATIONS_PATH)
